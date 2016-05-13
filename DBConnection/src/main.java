a27c57b9d2d11f862e430365d75de6048a3dfe19
lenis96 <lenisandres@hotmail.com>
2016-05-13 06:09:50 -0500

    prime subi con conexion a base de datos

---
+++ DBConnection/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DBConnection connection=new DBConnection();
+		System.out.println("bien");
+
+	}
+
+}
---
+++ sql/configdb.sql
@@ -0,0 +1,3 @@
+create database SIGEINM;
+create user 'SIGEINM'@'%' identified by 'SIGEINM';
+grant all on SIGEINM.* to 'SIGEINM';
